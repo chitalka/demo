95ac0c8e2ccb4bd6005cf49657050301e3dc9240
Oleg Mokhov <oleg.mokhov@gmail.com>
2026-08-19 15:15:56 +0300

    release: localize 2.01 attribution from reader 60d3cd8

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX = 'chitalka-shell-';
-const CACHE_NAME = CACHE_PREFIX + '08b5b4cf';
-const PRECACHE = ["./","./manifest.webmanifest","./manifest.ru.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png","./assets/Anna-Karenina-10258ZOi.fb2","./assets/index-BHfcQ_aC.js","./assets/index-Bgl1XMjZ.css"];
+const CACHE_NAME = CACHE_PREFIX + '8d0accef';
+const PRECACHE = ["./","./manifest.webmanifest","./manifest.ru.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png","./assets/Anna-Karenina-10258ZOi.fb2","./assets/index-BnCAaf3Z.js","./assets/index-DUIBEJZo.css"];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
