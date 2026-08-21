31b0f005b8cb2b0ec18d4f612da44548a79dd6cc
Oleg Mokhov <oleg.mokhov@gmail.com>
2026-08-21 23:34:59 +0300

    fix: update 2.02.01 from reader be8ff02

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX = 'chitalka-shell-';
-const CACHE_NAME = CACHE_PREFIX + 'bf4e594c';
-const PRECACHE = ["./","./manifest.webmanifest","./manifest.ru.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png","./assets/Anna-Karenina-10258ZOi.fb2","./assets/index-CgPQOP5c.js","./assets/index-RvVNV-0_.css"];
+const CACHE_NAME = CACHE_PREFIX + '2663009a';
+const PRECACHE = ["./","./manifest.webmanifest","./manifest.ru.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png","./assets/Anna-Karenina-10258ZOi.fb2","./assets/index-9kWDWm3F.js","./assets/index-RvVNV-0_.css"];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
