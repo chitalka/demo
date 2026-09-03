e1533def319620d795f8d2608d96d09e2d3e1a0a
Oleg Mokhov <oleg.mokhov@gmail.com>
2026-09-03 23:05:37 +0300

    release: publish 2.04 from reader 48aef06

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX = 'chitalka-shell-';
-const CACHE_NAME = CACHE_PREFIX + '1e714b8c';
-const PRECACHE = ["./","./manifest.webmanifest","./manifest.ru.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png","./assets/Anna-Karenina-10258ZOi.fb2","./assets/index-HBsIAqeJ.css","./assets/index-IsBFd8bA.js"];
+const CACHE_NAME = CACHE_PREFIX + '0add8ec2';
+const PRECACHE = ["./","./manifest.webmanifest","./manifest.ru.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png","./assets/Anna-Karenina-10258ZOi.fb2","./assets/index-BhqnR2Gp.js","./assets/index-CVA-EnLF.css"];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
